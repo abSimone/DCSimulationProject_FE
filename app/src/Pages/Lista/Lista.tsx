9c0be9b60e3ac0c08b326ce727918b3a9646106f
abSimone <s.angelonibei@digitality-consulting.com>
2022-05-17 16:20:16 +0200

    lista pizze + dettaglio pizza

--- app/src/Pages/Lista/Lista.tsx
+++ app/src/Pages/Lista/Lista.tsx
@@ -1,8 +1,45 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom"
+
+interface Pizza {
+  id_pizza: number;
+  nome: string;
+  costo: number;
+}
+
 const Lista = () => {
+
+  const getPizze = async () => {
+    const response = await fetch("http://127.0.0.1:8000/pizza",
+      {
+        headers: {
+          "content-type": "application/json",
+          accept: "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+      });
+   
+   const data = await response.json();
+   setPizze(data.data)
+    
+    
+  }
+
+  useEffect(() => { getPizze() }, []);
+
+  const [pizze, setPizze] = useState<Pizza[]>([]);
   return (
-    <>
+    <div>
       <h1>Lista</h1>
-    </>
+      {pizze.map((pizza, idx) => (
+        <>
+          <div className="pizze">
+            <Link to={`/dettaglio/${pizza.id_pizza}`}><div>{pizza.nome}</div></Link>
+            <div>{pizza.costo}</div>
+          </div>
+        </>
+      ))}
+    </div>
   );
 };
 export default Lista;
